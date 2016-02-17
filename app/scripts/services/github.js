fe182bea900c038d247a1489c34fbb82063c219d
Vishwajeet Vatharkar <vishwajeetvatharkar@gmail.com>
2016-02-17 17:02:05 +0530

    service created for retrieving github issues

--- app/scripts/services/github.js
+++ app/scripts/services/github.js
@@ -10,4 +10,21 @@
 angular.module('githubIssueTrackerApp')
   .service('Github', function ( $http , $q ) {
 
+      this.getIssues = function()
+      {
+          var url = 'https://api.github.com/repos/Shippable/support/issues';
+
+          var deferred = $q.defer();
+
+          $http.get(url).
+          success(function (response, status) {
+              deferred.resolve(response);
+          }).
+          error(function (data, status) {
+              deferred.reject(data);
+          });
+
+          return deferred.promise;
+      }
+
   });
