14306d060404859aa3513f69bed02144ca827610
Vishwajeet Vatharkar <vishwajeetvatharkar@gmail.com>
2016-02-17 16:08:42 +0530

    service created for github. About controller removed.

---
+++ app/scripts/services/github.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name githubIssueTrackerApp.Github
+ * @description
+ * # Github
+ * Service in the githubIssueTrackerApp.
+ */
+angular.module('githubIssueTrackerApp')
+  .service('Github', function ( $http , $q ) {
+
+  });
