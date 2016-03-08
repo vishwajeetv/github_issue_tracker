6fb30e06680b6f37f38f015b8911a96eabde3e6d
Vishwajeet Vatharkar <vishwajeetvatharkar@gmail.com>
2016-03-08 19:41:10 +0530

    fixed github service

--- app/scripts/services/github.js
+++ app/scripts/services/github.js
@@ -13,10 +13,8 @@ angular.module('githubIssueTrackerApp')
       this.getOpenIssues = function(repoURL)
       {
           var url = 'https://api.github.com/repos/'+repoURL;
-          var url = 'https://api.github.com/repos/Shippable/support';
 
           var deferred = $q.defer();
-
 
           $http.get(url).
           success(function (response, status) {
@@ -31,9 +29,7 @@ angular.module('githubIssueTrackerApp')
 
       this.getIssuesCreatedAfter = function(repoURL, date)
       {
-          var url = 'https://api.github.com/repos/'+repoURL;
-
-          var url = 'https://api.github.com/repos/Shippable/support/issues?since='+moment().subtract(1, 'days').format();
+          var url = 'https://api.github.com/repos/'+repoURL+'/issues?since='+moment().subtract(1, 'days').format();
 
           var deferred = $q.defer();
 
@@ -51,15 +47,26 @@ angular.module('githubIssueTrackerApp')
 
       this.getIssuesCreatedBefore = function(repoURL, date)
       {
-          var url = 'https://api.github.com/repos/'+repoURL;
-
-          //var url = 'https://api.github.com/search/q?repo=Shippable%2Fsupport&created:'+moment().subtract(7, 'days').format();
-
-
-          var url = 'https://api.github.com/search?utf8=%E2%9C%93&q=repo:Shippable/support&created:<'+moment().subtract(1000, 'days').calendar()+'&type=Issues';
+          var url = 'https://api.github.com/search/issues?q=repo:'+repoURL+' created:<'+moment().subtract(7, 'days').format('YYYY-MM-DD');
           var deferred = $q.defer();
-          //
-          //https://github.com/search?utf8=%E2%9C%93&q=repo%3Ashippable%2Fsupport+created%3A%3C2015-03-01&type=Issues&ref=searchresults
+          $http.get(url).
+          success(function (response, status) {
+              deferred.resolve(response);
+          }).
+          error(function (data, status) {
+              deferred.reject(data);
+          });
+
+          return deferred.promise;
+      }
+
+      this.getIssuesCreatedInBetween = function(repoURL, date)
+      {
+          var url = 'https://api.github.com/search/issues?q=repo:'+
+              repoURL+' created:"'+moment().subtract(7, 'days').format('YYYY-MM-DD')+
+              ' .. '+moment().subtract(1, 'days').format('YYYY-MM-DD')+'"';
+
+          var deferred = $q.defer();
           $http.get(url).
           success(function (response, status) {
               deferred.resolve(response);
