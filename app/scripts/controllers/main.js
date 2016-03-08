ab9942415a375ed84183b65d8f4bc5daff688f62
Vishwajeet Vatharkar <vishwajeetvatharkar@gmail.com>
2016-03-08 19:42:12 +0530

    controller fixed, all issue tracking functionality is now working

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,46 +9,45 @@
  */
 angular.module('githubIssueTrackerApp')
   .controller('MainCtrl', function ( $scope , Github) {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
 
       $scope.totalNumberOfIssues = 0;
       $scope.totalNumberOfIssuesLastDay = 0;
       $scope.totalNumberOfIssuesSevenDaysAgo = 0;
+      $scope.totalNumberOfIssuesInBetween = 0;
+
+      function urlCleaner(url) {
+          var repoURL = url.replace('http://github.com/', '');
+          repoURL = repoURL.replace('https://github.com/', '');
+
+          if (repoURL.substr(-1) === '/') {
+              repoURL = repoURL.substr(0, repoURL.length - 1);
+          }
+          return repoURL;
+      }
 
       $scope.getGitHubIssues = function(url)
       {
-          var repoURL = url.replace('http://github.com/','');
-          repoURL = repoURL.replace('https://github.com/','');
+          var repoURL = urlCleaner(url);
 
-          var repoURLLength = repoURL.length;
-          if(repoURL[repoURLLength - 1] != '/')
-          {
-              repoURL = repoURL+'/';
-          }
-      //
-      //    Github.getOpenIssues(repoURL).
-      //    then(function(response)
-      //        {
-      //            console.log(response);
-      //            $scope.totalNumberOfIssues = response.open_issues_count;
-      //        },
-      //        function (response) {
-      //
-      //        });
-      //
-      //    Github.getIssuesCreatedAfter(repoURL).
-      //    then(function(response)
-      //        {
-      //            console.log(response);
-      //            $scope.totalNumberOfIssuesLastDay = response.length;
-      //        },
-      //        function (response) {
-      //
-      //        });
+          Github.getOpenIssues(repoURL).
+          then(function(response)
+              {
+                  console.log(response);
+                  $scope.totalNumberOfIssues = response.open_issues_count;
+              },
+              function (response) {
+
+              });
+
+          Github.getIssuesCreatedAfter(repoURL).
+          then(function(response)
+              {
+                  console.log(response);
+                  $scope.totalNumberOfIssuesLastDay = response.length;
+              },
+              function (response) {
+
+              });
 
           Github.getIssuesCreatedBefore(repoURL).
           then(function(response)
@@ -56,6 +55,17 @@ angular.module('githubIssueTrackerApp')
                   console.log("issues created before 7 days");
                   console.log(response);
                   $scope.totalNumberOfIssuesSevenDaysAgo = response.total_count;
+              },
+              function (response) {
+
+              });
+
+          Github.getIssuesCreatedInBetween(repoURL).
+          then(function(response)
+              {
+                  console.log("issues created in between last 7 days");
+                  console.log(response);
+                  $scope.totalNumberOfIssuesInBetween = response.total_count;
               },
               function (response) {
 
